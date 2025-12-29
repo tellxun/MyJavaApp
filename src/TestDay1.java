f3dffec1a9148e59724c58d083dcd6fae49f0f5c
tellxun <redeog@163.com>
2025-12-29 11:21:24 +0800

    123

--- src/TestDay1.java
+++ src/TestDay1.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class TestDay1 {
     public static void main(String args[]){
         System.out.println("hello world");
+        System.out.println("zouxun123");
 
     }
 }
